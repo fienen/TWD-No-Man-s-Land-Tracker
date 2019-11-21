df3897d66ca73a26d12711b242c61d40e3fd4e20
Michael Fienen <fienen@gmail.com>
2019-11-20 20:15:54 -0600

    Init plugin base file

---
+++ no-mans-land-tracker.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: TWD No Man's Land Call Tracker
+ * Plugin URI: https://github.com/fienen/TWD-No-Man-s-Land-Tracker
+ * Description: 
+ * Version: 1.0
+ * Author: Michael Fienen
+ * Author URI: https://fienen.com
+ */
